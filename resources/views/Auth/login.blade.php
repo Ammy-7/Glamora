df627854ea6e379c2704710f125d082dd30ff512
Ammy-7 <amberyousuf20@gmail.com>
2025-11-29 06:11:44 -0800

    login page

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,3 +1,49 @@
-<div>
-    <!-- We must ship. - Taylor Otwell -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+<div class="container">
+    <div class="row">   
+        <div class="col-md-4"></div>
+            <div class="col-md-4">
+                @if(session('success'))
+                    <div class="alert alert-success" role="alert">
+{{session('success')}}
 </div>
+                @endif
+                
+                <h1>Welcome login</h1>
+                            <form action="{{route('login_user')}}" method="post">
+                        @csrf
+                            <input type="text" name="email" placeholder="enter email" class="form-control mt-4"value="{{ old('email') }}">
+                            @error('email')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                            
+                            <input type="text" name="password" placeholder="enter password" class="form-control mt-4"value="{{ old('password') }}">
+                            @error('password')
+                            <p class="text-danger">{{ $message }}</p>
+                        @enderror    
+                            
+                            <br>  
+                    <button type="submit" class="btn btn-primary">Login</button>
+
+
+                    </form>            
+            </div>       
+    </div>
+</div>
+     
+</body>
+</html> 
+
+
+
+
