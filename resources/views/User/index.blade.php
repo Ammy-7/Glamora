3497b57b4d35bde44e288040391399a77fbf5812
Ammy-7 <amberyousuf20@gmail.com>
2025-11-30 07:58:29 -0800

    update index

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -20,7 +20,7 @@
     <link href="user/https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="user/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="user/css/style.css" rel="stylesheet">
@@ -633,29 +633,29 @@
         <div class="row px-xl-5">
             <div class="col">
                 <div class="owl-carousel vendor-carousel">
-                    <div class="vendor-item border p-4">
-                        <img src="img/vendor-1.jpg" alt="">
+                    <div class="user/vendor-item border p-4">
+                        <img src="user/mg/vendor-1.jpg" alt="">
                     </div>
-                    <div class="vendor-item border p-4">
-                        <img src="img/vendor-2.jpg" alt="">
+                    <div class="user/vendor-item border p-4">
+                        <img src="user/img/vendor-2.jpg" alt="">
                     </div>
-                    <div class="vendor-item border p-4">
-                        <img src="img/vendor-3.jpg" alt="">
+                    <div class="user/vendor-item border p-4">
+                        <img src="user/img/vendor-3.jpg" alt="">
                     </div>
-                    <div class="vendor-item border p-4">
-                        <img src="img/vendor-4.jpg" alt="">
+                    <div class="user/vendor-item border p-4">
+                        <img src="user/img/vendor-4.jpg" alt="">
                     </div>
-                    <div class="vendor-item border p-4">
-                        <img src="img/vendor-5.jpg" alt="">
+                    <div class="user/vendor-item border p-4">
+                        <img src="user/img/vendor-5.jpg" alt="">
                     </div>
-                    <div class="vendor-item border p-4">
-                        <img src="img/vendor-6.jpg" alt="">
+                    <div class="user/vendor-item border p-4">
+                        <img src="user/img/vendor-6.jpg" alt="">
                     </div>
-                    <div class="vendor-item border p-4">
-                        <img src="img/vendor-7.jpg" alt="">
+                    <div class="user/vendor-item border p-4">
+                        <img src="user/img/vendor-7.jpg" alt="">
                     </div>
-                    <div class="vendor-item border p-4">
-                        <img src="img/vendor-8.jpg" alt="">
+                    <div class="user/vendor-item border p-4">
+                        <img src="user/img/vendor-8.jpg" alt="">
                     </div>
                 </div>
             </div>
